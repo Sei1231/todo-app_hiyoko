6d34dd7b73002481d3afa106d0933f82b03e996e
keisukeoba <keisuke1111oba@gmail.com>
2025-05-23 12:12:19 +0900

    変更

--- database/migrations/2025_05_23_030623_create_tasks_table.php
+++ database/migrations/2025_05_23_030623_create_tasks_table.php
@@ -9,13 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('tasks', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->date('time');           // 期限
+        $table->date('done_at')->nullable(); // 完了日時（null = 未完了）
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
