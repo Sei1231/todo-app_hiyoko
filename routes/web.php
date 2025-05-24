f638ba3dea9bc4d8d6070973141cf3dcc0955e45
Sei1231 <90814158+Sei1231@users.noreply.github.com>
2025-05-24 11:27:05 +0800

    新規登録紐付け完了

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< Updated upstream
 Route::get('/firstpage', function () {
     return view('firstpage');
 });
@@ -33,8 +32,6 @@ Route::get('/createMain', function () {
     return view('authMain.create');
 });
 
-=======
->>>>>>> Stashed changes
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
