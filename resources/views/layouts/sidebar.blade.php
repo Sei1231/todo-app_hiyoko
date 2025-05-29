a736a0e6874f19d4351d306fb37a4682600f872d
keisukeoba <keisuke1111oba@gmail.com>
2025-05-29 11:54:57 +0900

    ジャンル表示できるようになりました。

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,5 +9,16 @@
     <button class="btn btn-category btn-study">勉強</button>
     <button class="btn btn-category btn-family">家族</button>
 
+    <div class="sidebar">
+        <h2>ジャンルで絞り込み</h2>
+        @foreach ($kinds as $kind)
+            <a href="{{ route('tasks.filterByKind', $kind->id) }}" class="btn btn-category"
+                style="background-color: #{{ $kind->color_code }};">
+                {{ $kind->name }}
+            </a>
+        @endforeach
+    </div>
+
+
     <div class="settings"><i class="fa-solid fa-gear"></i>設定/テーマ変更</div>
 </aside>
