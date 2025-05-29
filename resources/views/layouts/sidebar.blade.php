8a0f266a206f0152543e350a8e4454c3086c55f4
keisukeoba <keisuke1111oba@gmail.com>
2025-05-29 12:25:52 +0900

    ジャンルにバックカラー。完了タスク一覧ボタン隠れてたけど出しました。

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,19 +1,14 @@
 <!-- Sidebar -->
-<aside class="sidebar">
-    <h2>{{ optional(auth()->user())->name }} さんのTO&nbsp;DO&nbsp;リスト</h2>
-    <button class="btn btn-template" onclick="location.href='{{ route('tasks.index') }}'">タスク一覧</button>
-    <button class="btn btn-done" onclick="location.href='{{ route('tasks.doneList') }}'">完了タスク一覧</button>
-
-    <h2>{{ optional(auth()->user())->name }} さんのジャンル</h2>
-    <button class="btn btn-category btn-work">仕事</button>
-    <button class="btn btn-category btn-study">勉強</button>
-    <button class="btn btn-category btn-family">家族</button>
-
     <div class="sidebar">
+
+        <h1>{{ optional(auth()->user())->name }} さんのTO&nbsp;DO&nbsp;リスト</h1>
+        <button class="btn btn-template" onclick="location.href='{{ route('tasks.index') }}'">タスク一覧</button>
+        <button class="btn btn-done" onclick="location.href='{{ route('tasks.doneList') }}'">完了タスク一覧</button>
+
         <h2>ジャンルで絞り込み</h2>
         @foreach ($kinds as $kind)
             <a href="{{ route('tasks.filterByKind', $kind->id) }}" class="btn btn-category"
-                style="background-color: #{{ $kind->color_code }};">
+                style="background-color: {{ $kind->color_code }};">
                 {{ $kind->name }}
             </a>
         @endforeach
